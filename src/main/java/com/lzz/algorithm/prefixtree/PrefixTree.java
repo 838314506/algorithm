2edf8bf699a87957d6db6ddea060fc0d837c3fa8
838314506 <zzhen.liu@eventmsimple.com>
2020-12-07 10:56:21 +0800

    前缀树

--- src/main/java/com/lzz/algorithm/prefixtree/PrefixTree.java
+++ src/main/java/com/lzz/algorithm/prefixtree/PrefixTree.java
@@ -1,4 +1,22 @@
 package com.lzz.algorithm.prefixtree;
 
 public class PrefixTree {
+
+    public static int getValue(int i) {
+        int result = 0;
+        switch (i) {
+            case 1:
+                result = result + i;
+            case 2:
+                result = result + i * 2;
+            case 3:
+                result = result + i * 3;
+        }
+        return result;
+    }
+
+    public static void main(String[] arg){
+        System.out.println(getValue(2));
+    }
+
 }
