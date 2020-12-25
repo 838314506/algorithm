1390487421bf75dd0a7f6c1ebb3d4536944988c9
838314506 <zzhen.liu@eventmsimple.com>
2020-12-25 16:47:36 +0800

    递归遍历二叉树

---
+++ src/main/java/com/lzz/algorithm/binaryTree/RecursiveTraversalBT.java
@@ -0,0 +1,60 @@
+package com.lzz.algorithm.binaryTree;
+
+/**
+ * 递归先、中、后遍历二叉树
+ */
+public class RecursiveTraversalBT {
+
+    public static class Node{
+        int value;
+        Node left;
+        Node right;
+
+        public Node(int value){
+            this.value = value;
+        }
+    }
+
+    public static void pre(Node head){
+        if (head == null){
+            return;
+        }
+
+        System.out.println(head.value);
+        pre(head.left);
+        pre(head.right);
+    }
+    public static void in(Node head){
+        if (head == null){
+            return;
+        }
+        pre(head.left);
+        System.out.println(head.value);
+        pre(head.right);
+    }
+    public static void pos(Node head){
+        if (head == null){
+            return;
+        }
+        pre(head.left);
+        pre(head.right);
+        System.out.println(head.value);
+    }
+
+    public static void main(String[] arg){
+        Node head = new Node(1);
+        head.left = new Node(2);
+        head.right = new Node(3);
+        head.left.left = new Node(4);
+        head.left.right = new Node(5);
+        head.right.left = new Node(6);
+        head.right.right = new Node(7);
+
+        pre(head);
+        System.out.println("========");
+        in(head);
+        System.out.println("========");
+        pos(head);
+        System.out.println("========");
+    }
+}
