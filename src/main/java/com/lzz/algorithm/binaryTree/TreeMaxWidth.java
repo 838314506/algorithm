dca976ba3556a6b2304e2ae7e8e3f8c7304a6de2
838314506 <zzhen.liu@eventmsimple.com>
2020-12-30 18:34:47 +0800

    二叉树获取最多宽层节点数

--- src/main/java/com/lzz/algorithm/binaryTree/TreeMaxWidth.java
+++ src/main/java/com/lzz/algorithm/binaryTree/TreeMaxWidth.java
@@ -1,5 +1,10 @@
 package com.lzz.algorithm.binaryTree;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
 /**
  * 获取二叉树最宽层，节点数量
  * 重点：建立层结束机制
@@ -20,6 +25,17 @@ public class TreeMaxWidth {
     public static int maxWidthUseMap(Node h){
         if (h == null) return 0;
         int max = 0;
+        int curLevelNodeCount = 0;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(h);
+        //node为key，层数为value
+        Map<Node,Integer> map = new HashMap<>();
+        map.put(h,1);
+        while (! queue.isEmpty()){
+
+        }
+
+
         return max;
     }
 
