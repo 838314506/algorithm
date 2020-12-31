3c74076ed7262bf1160059a1f3e7912f4efdacf3
838314506 <zzhen.liu@eventmsimple.com>
2020-12-31 14:02:05 +0800

    二叉树用map获取最宽层节点数

--- src/main/java/com/lzz/algorithm/binaryTree/TreeMaxWidth.java
+++ src/main/java/com/lzz/algorithm/binaryTree/TreeMaxWidth.java
@@ -25,6 +25,7 @@ public class TreeMaxWidth {
     public static int maxWidthUseMap(Node h){
         if (h == null) return 0;
         int max = 0;
+        int curLevel = 1;
         int curLevelNodeCount = 0;
         Queue<Node> queue = new LinkedList<>();
         queue.add(h);
@@ -32,9 +33,25 @@ public class TreeMaxWidth {
         Map<Node,Integer> map = new HashMap<>();
         map.put(h,1);
         while (! queue.isEmpty()){
-
+            Node currentNode = queue.poll();
+            int curNodeLevel = map.get(currentNode);
+            if (currentNode.left != null){
+                map.put(currentNode.left,curLevel + 1);
+                queue.add(currentNode.left);
+            }
+            if (currentNode.right != null){
+                map.put(currentNode.right,curLevel + 1);
+                queue.add(currentNode.right);
+            }
+            if (curNodeLevel == curLevel){
+                curLevelNodeCount ++;
+            }else {
+                max = Math.max(max,curLevelNodeCount);
+                curLevel ++;
+                curLevelNodeCount = 1;
+            }
         }
-
+        max = Math.max(max,curLevelNodeCount);
 
         return max;
     }
@@ -43,10 +60,42 @@ public class TreeMaxWidth {
     public static int maxWidthNoMap(Node h){
         if (h == null) return 0;
         int max = 0;
+        int curLevelCount = 0;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(h);
+        while (! queue.isEmpty()){
+
+        }
         return max;
     }
 
-    public static void main(String[] args){
+    // for test
+    public static Node generateRandomBST(int maxLevel, int maxValue) {
+        return generate(1, maxLevel, maxValue);
+    }
 
+    // for test
+    public static Node generate(int level, int maxLevel, int maxValue) {
+        if (level > maxLevel || Math.random() < 0.5) {
+            return null;
+        }
+        Node head = new Node((int) (Math.random() * maxValue));
+        head.left = generate(level + 1, maxLevel, maxValue);
+        head.right = generate(level + 1, maxLevel, maxValue);
+        return head;
+    }
+
+
+    public static void main(String[] args){
+        int maxLevel = 10;
+        int maxValue = 100;
+        int testTimes = 1000000;
+        for (int i = 0; i < testTimes; i++) {
+            Node head = generateRandomBST(maxLevel, maxValue);
+            if (maxWidthUseMap(head) != maxWidthNoMap(head)) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("finish!");
     }
 }
