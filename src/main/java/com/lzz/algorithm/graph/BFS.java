61d0cc672a24dd6c3fe5fdbea9b05a3922c26a6d
838314506 <zzhen.liu@eventmsimple.com>
2021-01-27 15:05:58 +0800

    图按宽度遍历，以及图的基本设计实体model

---
+++ src/main/java/com/lzz/algorithm/graph/BFS.java
@@ -0,0 +1,43 @@
+package com.lzz.algorithm.graph;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 图按宽度遍历
+ * 先遍历离我最近的一层，然后下一层，某一层有多个点，不要求顺序
+ */
+public class BFS {
+
+    public static void bfs(Node start){
+        if (start == null){
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        //用set来存储遍历过的数据，否则会导致死循环
+        HashSet set = new HashSet();
+        queue.add(start);
+        set.add(start);
+
+        while (!queue.isEmpty()){
+            Node cur = queue.poll();
+            System.out.println(cur.value);
+            for (Node next : cur.nexts){
+                if (!set.contains(next)){
+                    queue.add(next);
+                    set.add(next);
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] arg){
+        int[][] matrix = new int[][]{{5 , 0 , 7},{3 , 0,  1},{4 , 0,  6},{2 , 6,  1},{2 , 1,  3}};
+        Graph graph = GraphGenerator.createGraph(matrix);
+        BFS.bfs(graph.nodes.get(0));
+        System.out.println();
+    }
+}
---
+++ src/main/java/com/lzz/algorithm/graph/Edge.java
@@ -0,0 +1,16 @@
+package com.lzz.algorithm.graph;
+
+public class Edge {
+
+    public int weight;
+
+    public Node fromNode;
+
+    public Node toNode;
+
+    public Edge(int weight,Node fromNode,Node toNode){
+        this.weight = weight;
+        this.fromNode = fromNode;
+        this.toNode = toNode;
+    }
+}
